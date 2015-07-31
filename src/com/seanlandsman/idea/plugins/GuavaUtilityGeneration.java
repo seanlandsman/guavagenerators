77d2ef47de2115b4994b2dc916205a6e564a5925
Simon Kalt <kaltsimon@users.noreply.github.com>
2015-07-31 17:46:20 +0200

    Use `java.util.Objects` methods if the language level permits it.

--- src/com/seanlandsman/idea/plugins/GuavaUtilityGeneration.java
+++ src/com/seanlandsman/idea/plugins/GuavaUtilityGeneration.java
@@ -4,7 +4,13 @@ import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.actionSystem.DataKeys;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.module.Module;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.roots.LanguageLevelModuleExtensionImpl;
+import com.intellij.openapi.roots.LanguageLevelProjectExtension;
+import com.intellij.pom.java.LanguageLevel;
 import com.intellij.psi.*;
 import com.intellij.psi.codeStyle.JavaCodeStyleManager;
 import com.intellij.psi.util.PsiTreeUtil;
@@ -13,6 +19,9 @@ import java.util.List;
 
 public abstract class GuavaUtilityGeneration extends AnAction {
     public static final String COM_GOOGLE_COMMON_BASE_OBJECTS = "com.google.common.base.Objects";
+    public static final String JAVA_UTIL_OBJECTS = "java.util.Objects";
+    public static final LanguageLevel JAVA_UTIL_OBJECTS_VERSION = LanguageLevel.JDK_1_7;
+    protected boolean hasJavaUtilObjects = false;
     private String dialogTitle;
 
     public GuavaUtilityGeneration(String text, String dialogTitle) {
@@ -21,12 +30,31 @@ public abstract class GuavaUtilityGeneration extends AnAction {
     }
 
     public void actionPerformed(AnActionEvent e) {
+        hasJavaUtilObjects = findLanguageLevel(e).compareTo(JAVA_UTIL_OBJECTS_VERSION) >= 0;
+
         PsiClass psiClass = getPsiClassFromContext(e);
         GenerateDialog dlg = new GenerateDialog(psiClass, dialogTitle);
         dlg.show();
         if (dlg.isOK()) {
             generate(psiClass, dlg.getFields());
         }
+    }
+
+    private LanguageLevel findLanguageLevel(AnActionEvent e) {
+        Module module = DataKeys.MODULE.getData(e.getDataContext());
+        LanguageLevel languageLevel = LanguageLevel.JDK_1_6;
+
+        Project project = e.getProject();
+        if (project != null) {
+            languageLevel = LanguageLevelProjectExtension.getInstance(project).getLanguageLevel();
+        }
+
+        if (module != null) {
+            LanguageLevel moduleLanguageLevel = LanguageLevelModuleExtensionImpl.getInstance(module).getLanguageLevel();
+            languageLevel = moduleLanguageLevel != null ? moduleLanguageLevel : languageLevel;
+        }
+
+        return languageLevel;
     }
 
     abstract public void generate(final PsiClass psiClass, final List<PsiField> fields);
